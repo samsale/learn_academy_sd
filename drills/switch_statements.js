b8e0eeb7e4f06eae754702ab0023eb47a6756c52
Sam Sale <samsale@Sams-MacBook-Pro.local>
2018-12-05 08:58:51 -0800

    Updated Switch

--- drills/switch_statements.js
+++ drills/switch_statements.js
@@ -37,3 +37,18 @@ switch (new Date().getDay()) {
 }
 
 console.log(text);
+
+
+
+switch (new Date().getDay()) {
+  case 4:
+  case 5:
+    text = "Soon it is Weekend";
+    break;
+  case 0:
+  case 6:
+    text = "It is Weekend";
+    break;
+  default:
+    text = "Looking forward to the Weekend";
+}
