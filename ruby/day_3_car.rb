021e442796f13af0904e1b2e2343aeff80508100
Sam Sale <sam.sale@sky.com>
2019-01-09 14:45:11 -0800

    Day 3 Ruby Tests

---
+++ ruby/day_3_car.rb
@@ -0,0 +1,157 @@
+# Story: As a programmer, I can make a vehicle.
+# Hint: Test that Vehicle.new does not raise any errors.
+class Car
+  attr_reader :wheels, :make, :year, :lights_on, :signal, :speed, :model
+
+  def initialize (model)
+    @wheels = 4
+    @make = "Generic"
+    @model = model
+    @year = nil
+    @lights_on = false
+    @signal = "Off"
+    @speed = 0
+    @acceleration_rate = nil
+    @braking_rate = nil
+
+  end
+
+  def details
+    "The car is a #{@make}, it accelerates at #{@acceleration_rate} km/h and brakes at #{@braking_rate} km/h"
+  end
+
+  def accelerate
+    @speed = @speed + @acceleration_rate
+  end
+
+  def brake
+    @speed = @speed - @braking_rate
+  end
+
+  def honk
+    "BEEP"
+  end
+
+  def signal_left
+    @signal = "Left"
+  end
+
+  def signal_right
+    @signal = "Right"
+  end
+
+  def signal_off
+    @signal = "Off"
+  end
+
+  def toggle_lights
+    if @lights_on == false
+      @lights_on = true
+    else
+      @lights_on = false
+    end
+  end
+end
+
+class Toyota < Car
+  def initialize (model)
+    super
+    @make = "Toyota"
+    @year = 2019
+    @acceleration_rate = 7
+    @braking_rate = 5
+  end
+  def honk
+    super
+    "Whoop"
+  end
+end
+
+class Tata < Car
+  def initialize (model)
+    super
+    @make = "Tata"
+    @year = 2001
+    @acceleration_rate = 2
+    @braking_rate = 1.25
+  end
+end
+
+class Tesla < Car
+  def initialize (model)
+    super
+    @make = "Tesla"
+    @year = 2019
+    @acceleration_rate = 10
+    @braking_rate = 7
+  end
+
+  def honk
+    super
+    "Beep-bee-bee-boop-bee-doo-weep"
+  end
+end
+
+
+
+
+#
+# Story: As a programmer, I can make a car.
+# Hint: Test that creating an instance of the new class does not raise any errors.
+#
+# Story: As a programmer, I can tell how many wheels a car has; default is four.
+# Hint: initialize the car to have four wheels, then create a method to return the number of wheels.
+#
+# Story: As a programmer, I can honk the horn.
+# Hint: When I call honk_horn, I get a "BEEP!".
+#
+# Story: As a programmer, I can make a Toyota car.
+# Hint: Test that class Toyota which inherits from class Car can be created.
+#
+# Story: As a programmer, I can honk the horn of a Toyota car which makes the sound "whoop".
+#
+# Story: As a programmer, I can make a Tata car.
+#
+# Story: As a programmer, I can honk the horn of a Tata car - "beep".
+#
+# Story: As a programmer, I can make a Tesla car.
+#
+# Story: As a programmer, I can honk the horn of Tesla - "Beep-bee-bee-boop-bee-doo-weep".
+#
+# Story: As a programmer, I can tell which make year a vehicle is from. make years never change.
+# Hint: Make make year part of the initialization.
+#
+# Story: As a programmer, I can turn on and off the lights on a given Vehicle.
+# Hint: Create method(s) to allow programmer to turn lights on and off. Which class are the methods in?
+#
+# Story: As a programmer, I can determine if the lights are on or off. Lights start in the off position.
+#
+# Story: As a programmer, I can signal left and right. Turn signals starts off.
+#
+# Story: As a programmer, I can determine the speed of a car. Speed starts at 0 km/h.
+#
+# Story: As a programmer, I can speed my Teslas up by 10 km/h per acceleration.
+#
+# Story: As a programmer, I can slow my Teslas down by 7 km/h per braking.
+#
+# Story: As a programmer, I can speed my Tatas up by 2 km/h per acceleration.
+#
+# Story: As a programmer, I can slow my Tatas down by 1.25 km/h per braking.
+#
+# Story: As a programmer, I can speed my Toyotas up by 7 km/h per acceleration.
+#
+# Story: As a programmer, I can slow my Toyotas down by 5 km/h per braking.
+#
+# Story: As a programmer, I can call upon a car to tell me all it's information.
+# Hint: Implement to_s on one or more classes. You can call a super class's to_s with super.
+#
+# Story: As a programmer, I can store and retrieve all of my cars from a garage.
+# Hint: "Garage" is a noun, "store" and "retrieve" are verbs.
+#
+# Story: As a programmer, I can sort the cars in my garage based on make year.
+#
+# Story: As a programmer, I can sort the cars in my garage based on make.
+# Hint: Sort based on class name.
+#
+# Story: As a programmer, I can sort the cars in my garage based on make and then year.
+# Hint: Find out how the spaceship operator can help you with an array.
---
+++ ruby/day_3_car_spec.rb
@@ -0,0 +1,211 @@
+require "rspec"
+require_relative "day_3_car.rb"
+
+describe "Car" do
+  it "Check that Vehicle exists" do
+    expect {Car.new("Generic Model")}.not_to raise_error
+  end
+
+  it "Check that I can create a new Class of car" do
+    a_car = Car.new("Generic Model")
+    expect(Car).to be_a(Class)
+    expect(a_car).to be_instance_of(Car)
+  end
+
+  it "Check the number of wheels of a car" do
+    a_car = Car.new("Generic Model")
+    expect(a_car.wheels).to be 4
+  end
+
+  it "Beeps when I honk horn" do
+    a_car = Car.new("Generic Model")
+    expect(a_car.honk).to eq("BEEP")
+  end
+
+it "Check to test creation of Toyota class" do
+  a_toyota = Toyota.new("Camry")
+  p a_toyota
+  expect(Toyota).to be_a(Class)
+  expect(Toyota.superclass).to eq(Car)
+  expect(a_toyota.make).to eq("Toyota")
+end
+
+it "Check Toyota makes whoop sound when honked" do
+  a_toyota = Toyota.new("Camry")
+  expect(a_toyota.honk).to eq("Whoop")
+  a_car = Car.new("Generic Model")
+  expect(a_car.honk).to eq("BEEP")
+end
+
+it "Check I can create a Tata Car" do
+  a_tata = Tata.new("Hexa")
+  expect(Tata).to be_a(Class)
+  expect(a_tata).to be_a(Tata)
+  expect(Tata.superclass).to eq(Car)
+  expect(a_tata.make).to eq("Tata")
+end
+
+it "Check I can honk horn of Tata and it beeps" do
+  a_tata = Tata.new("Hexa")
+  expect(a_tata.honk).to eq("BEEP")
+end
+
+
+it "Check I can create a Tesla class" do
+  a_tesla = Tesla.new("Model 3")
+  expect(Tesla).to be_a(Class)
+  expect(a_tesla).to be_a(Tesla)
+  expect(Tesla.superclass).to eq(Car)
+  expect(a_tesla.make).to eq("Tesla")
+  end
+
+  it "Check I can honk horn of Tata and it beeps" do
+    a_tesla = Tesla.new("Model 3")
+    expect(a_tesla.honk).to eq("Beep-bee-bee-boop-bee-doo-weep")
+  end
+
+it "Check the years of different car classes" do
+  a_tesla = Tesla.new("Model 3")
+  a_tata = Tata.new("Hexa")
+  a_toyota = Toyota.new("Camry")
+  expect(a_tesla.year).to be 2019
+  expect(a_tata.year).to be 2001
+  expect(a_toyota.year).to be 2019
+end
+
+it "Check the lights turn on and off on given vehicle" do
+  a_car = Car.new("Generic Model")
+  a_toyota = Toyota.new("Camry")
+  expect(a_car.lights_on).to eq(false)
+  expect(a_toyota.lights_on).to eq(false)
+  a_car.toggle_lights
+  expect(a_car.lights_on).to eq(true)
+  a_car.toggle_lights
+  expect(a_car.lights_on).to eq(false)
+end
+
+it "Check status of lights" do
+  a_car = Car.new("Generic Model")
+  a_tesla = Tesla.new("Model 3")
+  expect(a_car.lights_on).to eq(false)
+  expect(a_tesla.lights_on).to eq(false)
+  end
+
+
+it "To test the signal method on a car classes" do
+  a_car = Car.new("Generic Model")
+  expect(a_car.signal).to eq("Off")
+  a_car.signal_left
+  expect(a_car.signal).to eq("Left")
+  a_car.signal_right
+  expect(a_car.signal).to eq("Right")
+  a_car.signal_off
+  expect(a_car.signal).to eq("Off")
+end
+
+it "Check to determine the speed of the car" do
+  a_car = Car.new("Generic Model")
+  expect(a_car.speed).to be 0
+end
+
+it "Check to accelerate Tesla by 10 km/h" do
+  a_tesla = Tesla.new("Model 3")
+  expect(a_tesla.speed).to be 0
+  a_tesla.accelerate
+  expect(a_tesla.speed).to be 10
+end
+
+it "Check that the Telsa breaks at a rate of 7km/h" do
+  a_tesla = Tesla.new("Model 3")
+  a_tesla.accelerate
+  expect(a_tesla.speed).to be 10
+  a_tesla.brake
+  expect(a_tesla.speed).to be 3
+end
+
+it "Check to accelerate Tata by 2 km/h" do
+  a_tata = Tata.new("Hexa")
+  expect(a_tata.speed).to be 0
+  a_tata.accelerate
+  expect(a_tata.speed).to be 2
+  a_tata.accelerate
+  a_tata.accelerate
+  expect(a_tata.speed).to be 6
+end
+
+it "Check that the Tatas breaks at a rate of 1.25km/h" do
+  a_tata = Tata.new("Hexa")
+  a_tata.accelerate
+  a_tata.accelerate
+  expect(a_tata.speed).to be 4
+  a_tata.brake
+  expect(a_tata.speed).to be 2.75
+  a_tata.brake
+  expect(a_tata.speed).to be 1.5
+end
+
+it "Check to accelerate Toyota by 7 km/h" do
+  a_toyota = Toyota.new("Camry")
+  expect(a_toyota.speed).to be 0
+  a_toyota.accelerate
+  expect(a_toyota.speed).to be 7
+  a_toyota.accelerate
+  a_toyota.accelerate
+  expect(a_toyota.speed).to be 21
+end
+
+it "Check that the Toyota breaks at a rate of 5km/h" do
+  a_toyota = Toyota.new("Camry")
+  a_toyota.accelerate
+  a_toyota.accelerate
+  expect(a_toyota.speed).to be 14
+  a_toyota.brake
+  expect(a_toyota.speed).to be 9
+  a_toyota.brake
+  expect(a_toyota.speed).to be 4
+end
+
+it "To test getting information on the car" do
+  a_toyota = Toyota.new("Camry")
+  a_tesla = Tesla.new("Model 3")
+  a_tata = Tata.new("Hexa")
+  toyota_details = a_toyota.details
+  expect(toyota_details).to be_a_kind_of(String)
+  expect(a_toyota.details).to eq("The car is a Toyota, it accelerates at 7 km/h and brakes at 5 km/h")
+  expect(a_tata.details).to eq("The car is a Tata, it accelerates at 2 km/h and brakes at 1.25 km/h")
+  expect(a_tesla.details).to eq("The car is a Tesla, it accelerates at 10 km/h and brakes at 7 km/h")
+  end
+
+it "Check the Cars are stored in an array, Garage"do
+my_cars = [a_toyota = Toyota.new("Camry"), a_tesla = Tesla.new("Model 3"), a_tata = Tata.new("Hexa")]
+  expect(my_cars).to be_a_kind_of(Array)
+  expect(my_cars[0].model).to eq("Camry")
+  expect(my_cars[2].speed).to be 0
+  expect(my_cars.length).to be 3
+end
+
+it "To make sure we can sort an array of car objects by year" do
+  my_cars = [a_toyota = Toyota.new("Camry"), a_tesla = Tesla.new("Model 3"), a_tata = Tata.new("Hexa")]
+  my_cars.sort_by!{|object| object.year}
+  expect(my_cars[0].year).to be 2001
+  expect(my_cars[2].make).to eq("Tesla")
+  expect(my_cars[1].model).to eq("Camry")
+  end
+
+it "To make sure we can sort array by make" do
+  my_cars = [a_toyota = Toyota.new("Camry"), a_tesla = Tesla.new("Model 3"), a_tata = Tata.new("Hexa")]
+  my_cars.sort_by!{|object| object.make}
+  expect(my_cars[0].make).to eq("Tata")
+  expect(my_cars[0].year).to be 2001
+end
+
+it "To make sure we can sort array by make and then year" do
+  my_cars = [a_toyota = Toyota.new("Camry"), a_tesla = Tesla.new("Model 3"), a_tata = Tata.new("Hexa")]
+  my_cars.sort_by!{|object| [object.make, object.year]}
+  expect(my_cars[0].make).to eq("Tata")
+  expect(my_cars[0].year).to be 2001
+  expect(my_cars[2].make).to eq("Toyota")
+  expect(my_cars[2].year).to be 2019
+end
+
+end
